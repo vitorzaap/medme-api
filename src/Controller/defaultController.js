2545749dd8377337d449a6921a6ff90221cba9b8
vitorzap <menezes.victorsantos@outlook.com>
2022-11-11 17:08:22 -0300

    default controller

--- src/Controller/defaultController.js
+++ src/Controller/defaultController.js
@@ -3,12 +3,8 @@ const router = Router();
 import { con } from "../Repo/connection.js";
 
 router.get("/", async (req, res) => {
-	const c = `
-    SELECT * FROM tb_usuario
-    `;
-	const [r] = await con.query(c);
 
-	res.send(r);
+	res.send("ping");
 });
 
 router.get("/ping", (req, res) => {
