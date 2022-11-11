ed1dbdd0125d6a387a5a33053c06e60dabc05cf4
vitorzap <menezes.victorsantos@outlook.com>
2022-11-11 15:42:54 -0300

    host

--- src/Repo/connection.js
+++ src/Repo/connection.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 const con = await mysql.createConnection({
-	host: process.env.DATABASE_URL,
+	host: process.env.DATABASE_HOST,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
