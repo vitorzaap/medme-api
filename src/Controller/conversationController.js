ea3bbb8704aa717c44091833b40d49ebbbed1d9f
vitorzap <menezes.victorsantos@outlook.com>
2022-11-11 16:56:36 -0300

    update

--- src/Controller/conversationController.js
+++ src/Controller/conversationController.js
@@ -1,64 +1,64 @@
-// import { Router } from "express";
-// import { userConversations, doctorConversation,createConversation, searchConversationbyId } from "../Repo/conversationRepo.js";
+import { Router } from "express";
+import { userConversations, doctorConversation,createConversation, searchConversationbyId } from "../Repo/conversationRepo.js";
 
-// const router = Router();
-// router.get('/conversation/search', async (req, res) => {
-//     try {
-//         const { id } = req.query;
-//         const r = await searchConversationbyId(id);
-//         res.send(r)
-//     } catch (err) {
-//         res.status(401).send({
-//             erro: err.message
-//         })
-//     }
-// })
-// router.get("/conversation", async (req, res) => {
-// 	try {
-//         const { doctorId, userId } = req.query;
-//         if (!doctorId && !userId) {
-//             throw new Error("você não passou nenhum parâmetro.")
-//         }
-// 		if (doctorId && !userId) {
-//             const r = await doctorConversation(Number(doctorId));
-//             if (!r || r == undefined) {
-//                 throw new Error("Nenhum cliente criou uma conversa ainda.")
-//             }
-//             else {
-//                 res.send(r);
-//             }
+const router = Router();
+router.get('/conversation/search', async (req, res) => {
+    try {
+        const { id } = req.query;
+        const r = await searchConversationbyId(id);
+        res.send(r)
+    } catch (err) {
+        res.status(401).send({
+            erro: err.message
+        })
+    }
+})
+router.get("/conversation", async (req, res) => {
+	try {
+        const { doctorId, userId } = req.query;
+        if (!doctorId && !userId) {
+            throw new Error("você não passou nenhum parâmetro.")
+        }
+		if (doctorId && !userId) {
+            const r = await doctorConversation(Number(doctorId));
+            if (!r || r == undefined) {
+                throw new Error("Nenhum cliente criou uma conversa ainda.")
+            }
+            else {
+                res.send(r);
+            }
 			
-// 		} else if (userId && !doctorId) {
-//             const r = await userConversations(Number(userId));
-//             if (!r || r == undefined) {
-//                 throw new Error("Você não tem nenhuma conversa.")
-//             }
-//             else {
-//                 res.send(r);
-//             }
+		} else if (userId && !doctorId) {
+            const r = await userConversations(Number(userId));
+            if (!r || r == undefined) {
+                throw new Error("Você não tem nenhuma conversa.")
+            }
+            else {
+                res.send(r);
+            }
 			
-//         }
-//         else if (userId && doctorId) {
-//             throw new Error("Você não pode passar os 2 parâmetros ao mesmo tempo.")
-//         }
-//     } catch (err) {
-//         res.status(401).send({
-//             erro: err.message
-//         })
-//     }
-// });
+        }
+        else if (userId && doctorId) {
+            throw new Error("Você não pode passar os 2 parâmetros ao mesmo tempo.")
+        }
+    } catch (err) {
+        res.status(401).send({
+            erro: err.message
+        })
+    }
+});
 
-// router.post("/conversation", async (req, res) => {
-//     try {
-//         const { doctorId, userId } = req.query;
-//         const r = await createConversation(Number(doctorId), Number(userId))
-//         res.sendStatus(200)
-//     }
-//     catch (err) {
-//         res.status(401).send({
-//             erro: err.message
-//         })
-//     }
-// })
+router.post("/conversation", async (req, res) => {
+    try {
+        const { doctorId, userId } = req.query;
+        const r = await createConversation(Number(doctorId), Number(userId))
+        res.sendStatus(200)
+    }
+    catch (err) {
+        res.status(401).send({
+            erro: err.message
+        })
+    }
+})
 
-// export default router;
+export default router;
--- src/Controller/messagesController.js
+++ src/Controller/messagesController.js
@@ -1,30 +1,30 @@
-// import { Router } from "express";
-// import { listMessage, sendMessage } from "../Repo/messagesRepo.js";
-// const router = Router();
+import { Router } from "express";
+import { listMessage, sendMessage } from "../Repo/messagesRepo.js";
+const router = Router();
 
-// router.get("/messages", async (req, res) => {
-// 	try {
-// 		const { conversation } = req.query;
-//         const r = await listMessage(Number(conversation));
-//         res.send(r)
-// 	} catch (err) {
-// 		res.send({
-// 			erro: err.message,
-// 		});
-// 	}
-// });
+router.get("/messages", async (req, res) => {
+	try {
+		const { conversation } = req.query;
+        const r = await listMessage(Number(conversation));
+        res.send(r)
+	} catch (err) {
+		res.send({
+			erro: err.message,
+		});
+	}
+});
 
-// router.post("/messages", async (req, res) => {
-//     try {
-//         const { conversation, type, senderId } = req.query
-//         const { message } = req.body;
-//         const r = await sendMessage(type, conversation, message, senderId)
-//         res.sendStatus(200);
-//     } catch (err) {
-//         res.send({
-//             erro: err.message
-//         })
-//     }
-// });
+router.post("/messages", async (req, res) => {
+    try {
+        const { conversation, type, senderId } = req.query
+        const { message } = req.body;
+        const r = await sendMessage(type, conversation, message, senderId)
+        res.sendStatus(200);
+    } catch (err) {
+        res.send({
+            erro: err.message
+        })
+    }
+});
 
-// export default router;
+export default router;
