75c5455d7eb71f2fec2dd72e0d0fc4d47bfee340
vitorzap <menezes.victorsantos@outlook.com>
2022-11-12 21:56:52 -0300

    fs and ssl certificate on mysql2

--- src/Repo/connection.js
+++ src/Repo/connection.js
@@ -1,9 +1,13 @@
 import mysql from "mysql2/promise";
+const fs = require('fs')
 
 const con = await mysql.createConnection({
 	host: "us-cdbr-east-06.cleardb.net",
 	user: "b0ede638fc1920",
 	password: "3dee67fa",
 	database: "heroku_f569236ee4e3c79",
+	ssl: {
+		cert: fs.readFileSync('./rds-ca-2019-root.pem')
+	  }
 });
 export { con };
